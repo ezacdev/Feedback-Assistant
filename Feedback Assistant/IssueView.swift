8834ad847c5437bee9929ad41a366b771734f615
ezacd <yaroslav.us2312@gmail.com>
2025-12-22 17:16:39 +0300

    feat: add issue detail view with editable form and tag menu

---
+++ Feedback Assistant/IssueView.swift
@@ -0,0 +1,8 @@
+//
+//  IssueView.swift
+//  Feedback Assistant
+//
+//  Created by Yaroslav Pleskach on 12/22/25.
+//
+
+import Foundation
---
+++ Feedback Assistant/NoIssueView.swift
@@ -0,0 +1,8 @@
+//
+//  NoIssueView.swift
+//  Feedback Assistant
+//
+//  Created by Yaroslav Pleskach on 12/22/25.
+//
+
+import Foundation
